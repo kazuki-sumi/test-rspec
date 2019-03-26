c5b504d3ebdccf596d9bfaadbb67925203e61936
Your Name <ec2-user@ip-172-31-4-123.us-east-2.compute.internal>
2019-03-26 07:24:48 +0000

    Add message_filter.rb

--- message_filter.rb
+++ message_filter.rb
@@ -1,9 +1,2 @@
 class MessageFilter
-  def initialize(*words)
-    @ng_words = words
-  end
-  attr_reader :ng_words
-  def detect?(text)
-    @ng_words.any?{|w| text.include?(w) }
-  end
 end
--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,22 +1,5 @@
+# 相対パス的にrequire
 require_relative 'message_filter'
-require 'rspec/its'
-require 'rspec/collection_matchers'
-
 describe MessageFilter do
-  shared_examples 'MessageFilter with argument "foo"' do
-    it { is_expected.to be_detect('hello from foo') }
-    it { is_expected.not_to be_detect('hello, world!') }
-    its(:ng_words) { is_expected.not_to be_empty }
-  end
-  context 'with argument "foo"' do
-    subject { MessageFilter.new('foo') }
-    it_behaves_like 'MessageFilter with argument "foo"'
-    it { is_expected.to have(1).ng_words }
-  end
-  context 'with argument "foo","bar"' do
-    subject { MessageFilter.new('foo', 'bar') }
-    it { is_expected.to be_detect('hello from bar') }
-    it_behaves_like 'MessageFilter with argument "foo"'
-    it { is_expected.to have(2).ng_words }
-  end
+  
 end
