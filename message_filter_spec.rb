33a7626bd44bb8770fcdc3f4b74a163a86443a81
Your Name <ec2-user@ip-172-31-4-123.us-east-2.compute.internal>
2019-03-27 05:34:19 +0000

    課題1の終わり

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,15 +1,7 @@
 # 相対パス的にrequire
 require_relative 'message_filter'
 describe MessageFilter, 'with argument "foo"' do
-  before do
-    @filter = MessageFilter.new('foo')
-  end
-  subject {@filter}
-  it{
-    is_expected.to be_detect('hello from foo')
-  }
-  
-  it{
-    is_expected.to_not be_detect('hello,world')
-  }
+  subject {MessageFilter.new('foo')}
+  it{is_expected.to be_detect('hello from foo')}
+  it{is_expected.to_not be_detect('hello,world')}
 end
