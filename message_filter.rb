72d0540ebd5b46f2173ffad7b310dc9e8b447306
Your Name <ec2-user@ip-172-31-4-123.us-east-2.compute.internal>
2019-03-27 04:52:34 +0000

    Omit spec descriptions

--- message_filter.rb
+++ message_filter.rb
@@ -1,2 +1,10 @@
 class MessageFilter
+  def initialize(word)
+    @word = word
+  end
+  
+  def detect?(text)
+    # 引数のtextと生成されたインスタンス変数との中身が少しでも含まれているか
+    text.include?(@word)
+  end
 end
--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,5 +1,14 @@
 # 相対パス的にrequire
 require_relative 'message_filter'
 describe MessageFilter do
+  before do
+    @filter = MessageFilter.new('foo')
+  end
+  it{
+    expect(@filter).to be_detect('hello from foo')
+  }
   
+  it{
+    expect(@filter).to_not be_detect('hello,world')
+  }
 end
