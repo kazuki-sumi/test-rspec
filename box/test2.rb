ba94f9ff266516a9023772265156c572fcaac26f
Your Name <ec2-user@ip-172-31-4-123.us-east-2.compute.internal>
2019-03-29 06:13:22 +0000

    commit5

--- box/test2.rb
+++ box/test2.rb
@@ -1,5 +1,5 @@
-ここはtest1の部分
-ここはtest1の部分
-ここはtest1の部分
-ここはtest1の部分
-ここはtest1の部分
+ここはtest1の部分ここはtest1の部分
+ここはtest1の部分ここはtest1の部分
+ここはtest1の部分ここはtest1の部分
+ここはtest1の部分ここはtest1の部分
+ここはtest1の部分ここはtest1の部分
